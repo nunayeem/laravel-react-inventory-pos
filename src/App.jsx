bc1cf6457d57b7400c05bfff263f147c4827ad3f
Nizam Uddin Nayeem <nizamuddinnayeem22@gmail.com>
2025-11-15 19:06:52 +0600

    Bootstrap Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import './assets/css/styles.css'
+import '@bootstrap/dist/css/bootstrap.min.css'
+import '@bootstrap/dist/js/bootstrap.bundle'
 import Footer from './components/partials/Footer';
 import Nav from './components/partials/Nav';
 import Sidebar from './components/partials/Sidebar';
@@ -13,75 +15,75 @@ function App() {
 
         <div id="layoutSidenav_content">
           <main>
-              <div class="container-fluid px-4">
-                  <h1 class="mt-4">Dashboard</h1>
-                  <ol class="breadcrumb mb-4">
-                      <li class="breadcrumb-item active">Dashboard</li>
+              <div className="container-fluid px-4">
+                  <h1 className="mt-4">Dashboard</h1>
+                  <ol className="breadcrumb mb-4">
+                      <li className="breadcrumb-item active">Dashboard</li>
                   </ol>
-                  <div class="row">
-                      <div class="col-xl-3 col-md-6">
-                          <div class="card bg-primary text-white mb-4">
-                              <div class="card-body">Primary Card</div>
-                              <div class="card-footer d-flex align-items-center justify-content-between">
-                                  <a class="small text-white stretched-link" href="#">View Details</a>
-                                  <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                  <div className="row">
+                      <div className="col-xl-3 col-md-6">
+                          <div className="card bg-primary text-white mb-4">
+                              <div className="card-body">Primary Card</div>
+                              <div className="card-footer d-flex align-items-center justify-content-between">
+                                  <a className="small text-white stretched-link" href="#">View Details</a>
+                                  <div className="small text-white"><i className="fas fa-angle-right"></i></div>
                               </div>
                           </div>
                       </div>
-                      <div class="col-xl-3 col-md-6">
-                          <div class="card bg-warning text-white mb-4">
-                              <div class="card-body">Warning Card</div>
-                              <div class="card-footer d-flex align-items-center justify-content-between">
-                                  <a class="small text-white stretched-link" href="#">View Details</a>
-                                  <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                      <div className="col-xl-3 col-md-6">
+                          <div className="card bg-warning text-white mb-4">
+                              <div className="card-body">Warning Card</div>
+                              <div className="card-footer d-flex align-items-center justify-content-between">
+                                  <a className="small text-white stretched-link" href="#">View Details</a>
+                                  <div className="small text-white"><i className="fas fa-angle-right"></i></div>
                               </div>
                           </div>
                       </div>
-                      <div class="col-xl-3 col-md-6">
-                          <div class="card bg-success text-white mb-4">
-                              <div class="card-body">Success Card</div>
-                              <div class="card-footer d-flex align-items-center justify-content-between">
-                                  <a class="small text-white stretched-link" href="#">View Details</a>
-                                  <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                      <div className="col-xl-3 col-md-6">
+                          <div className="card bg-success text-white mb-4">
+                              <div className="card-body">Success Card</div>
+                              <div className="card-footer d-flex align-items-center justify-content-between">
+                                  <a className="small text-white stretched-link" href="#">View Details</a>
+                                  <div className="small text-white"><i className="fas fa-angle-right"></i></div>
                               </div>
                           </div>
                       </div>
-                      <div class="col-xl-3 col-md-6">
-                          <div class="card bg-danger text-white mb-4">
-                              <div class="card-body">Danger Card</div>
-                              <div class="card-footer d-flex align-items-center justify-content-between">
-                                  <a class="small text-white stretched-link" href="#">View Details</a>
-                                  <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                      <div className="col-xl-3 col-md-6">
+                          <div className="card bg-danger text-white mb-4">
+                              <div className="card-body">Danger Card</div>
+                              <div className="card-footer d-flex align-items-center justify-content-between">
+                                  <a className="small text-white stretched-link" href="#">View Details</a>
+                                  <div className="small text-white"><i className="fas fa-angle-right"></i></div>
                               </div>
                           </div>
                       </div>
                   </div>
-                  <div class="row">
-                      <div class="col-xl-6">
-                          <div class="card mb-4">
-                              <div class="card-header">
-                                  <i class="fas fa-chart-area me-1"></i>
+                  <div className="row">
+                      <div className="col-xl-6">
+                          <div className="card mb-4">
+                              <div className="card-header">
+                                  <i className="fas fa-chart-area me-1"></i>
                                   Area Chart Example
                               </div>
-                              <div class="card-body"><canvas id="myAreaChart" width="100%" height="40"></canvas></div>
+                              <div className="card-body"><canvas id="myAreaChart" width="100%" height="40"></canvas></div>
                           </div>
                       </div>
-                      <div class="col-xl-6">
-                          <div class="card mb-4">
-                              <div class="card-header">
-                                  <i class="fas fa-chart-bar me-1"></i>
+                      <div className="col-xl-6">
+                          <div className="card mb-4">
+                              <div className="card-header">
+                                  <i className="fas fa-chart-bar me-1"></i>
                                   Bar Chart Example
                               </div>
-                              <div class="card-body"><canvas id="myBarChart" width="100%" height="40"></canvas></div>
+                              <div className="card-body"><canvas id="myBarChart" width="100%" height="40"></canvas></div>
                           </div>
                       </div>
                   </div>
-                  <div class="card mb-4">
-                      <div class="card-header">
-                          <i class="fas fa-table me-1"></i>
+                  <div className="card mb-4">
+                      <div className="card-header">
+                          <i className="fas fa-table me-1"></i>
                           DataTable Example
                       </div>
-                      <div class="card-body">
+                      <div className="card-body">
                           <table id="datatablesSimple">
                               <thead>
                                   <tr>
