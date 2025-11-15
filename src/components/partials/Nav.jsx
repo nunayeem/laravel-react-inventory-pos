362aa5b807cbe977c7e2549385f2d9aa35e2b7c7
Nizam Uddin Nayeem <nizamuddinnayeem22@gmail.com>
2025-11-15 19:24:32 +0600

    Jquery setup

--- src/components/partials/Nav.jsx
+++ src/components/partials/Nav.jsx
@@ -1,13 +1,17 @@
 import "../../assets/css/styles.css";
+import $ from 'jquery'
 
 function Nav() {
+    const handleSidebar = () => {
+        $('body').toggleClass('sb-sidenav-toggled')
+    }
   return (
     <>
       <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
             {/* <!-- Navbar Brand--> */}
             <a class="navbar-brand ps-3" href="index.html">Start Bootstrap</a>
             {/* <!-- Sidebar Toggle--> */}
-            <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
+            <button onClick={handleSidebar} class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
             {/* <!-- Navbar Search--> */}
             <form class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
                 <div class="input-group">
