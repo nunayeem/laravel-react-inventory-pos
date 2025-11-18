e8a8a98dec96b3af850f1f1ade83bc981a71d4ed
Nizam Uddin Nayeem <nizamuddinnayeem22@gmail.com>
2025-11-19 01:45:14 +0600

    Router Added

--- src/components/modules/category/CategoryAdd.jsx
+++ src/components/modules/category/CategoryAdd.jsx
@@ -1,51 +1,28 @@
 import React from 'react'
 import BreadCrumb from '../../partials/BreadCrumb'
+import { Link } from 'react-router-dom'
 
 const CategoryAdd = () => {
   return (
     <div>
       <BreadCrumb title={'Add Category'} />
       <div className="row">
-        <div className="col-xl-3 col-md-6">
-          <div className="card bg-primary text-white mb-4">
-            <div className="card-body">Primary Card</div>
-            <div className="card-footer d-flex align-items-center justify-content-between">
-              <a className="small text-white stretched-link" href="#">
-                View Details
-              </a>
-              <div className="small text-white">
-                <i className="fas fa-angle-right"></i>
-              </div>
+
+        <div className="col-md-12">
+          <div className="card">
+            <div className='card-header flex'>
+                <div className='d-flex align-items-center justify-content-between'> 
+                    <h5>Add Category</h5>
+                    <Link to={'/category'} className='btn btn-primary'>List</Link>
+                </div>
             </div>
+            <div className="card-body">
+
+            </div>
+
           </div>
         </div>
-        
-        <div className="col-xl-3 col-md-6">
-          <div className="card bg-success text-white mb-4">
-            <div className="card-body">Success Card</div>
-            <div className="card-footer d-flex align-items-center justify-content-between">
-              <a className="small text-white stretched-link" href="#">
-                View Details
-              </a>
-              <div className="small text-white">
-                <i className="fas fa-angle-right"></i>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="col-xl-3 col-md-6">
-          <div className="card bg-danger text-white mb-4">
-            <div className="card-body">Danger Card</div>
-            <div className="card-footer d-flex align-items-center justify-content-between">
-              <a className="small text-white stretched-link" href="#">
-                View Details
-              </a>
-              <div className="small text-white">
-                <i className="fas fa-angle-right"></i>
-              </div>
-            </div>
-          </div>
-        </div>
+
       </div>
     </div>
   )
