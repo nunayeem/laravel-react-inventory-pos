14315d9c83dbaeead9caae0cccaa8935057ecf27
Nizam Uddin Nayeem <nizamuddinnayeem22@gmail.com>
2025-11-22 23:02:44 +0600

    Base url

--- src/config/Config.js
+++ src/config/Config.js
@@ -1,5 +1,6 @@
 const Config =  {
-    BASE_URL : 'http://127.0.0.1:8000/api'
+    BASE_URL : 'https://pos.ilmunnur.com/api',
+    LocalBASE_URL : 'http://127.0.0.1:8000/api'
 }
 
 export default Config
